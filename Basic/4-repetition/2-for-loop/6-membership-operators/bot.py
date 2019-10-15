1e29896c978c2e684d3d64787713baef1d32e62b
BaDMaN90 <4zdebf99@solent.ac.uk>
2019-10-15 11:09:59 +0000

    ver 1.00

--- Basic/4-repetition/2-for-loop/6-membership-operators/bot.py
+++ Basic/4-repetition/2-for-loop/6-membership-operators/bot.py
@@ -1,5 +1,13 @@
-phrase = "hello"
-word = ""
-for meh in phrase:
-    word = word + meh
-print(word)
+# Ask user for phrase
+print("What phrase do you see?")
+phrase = input()
+
+# Identify markings
+print("\nReversing...\nThe phrase is", end="")
+
+reversed = ""
+
+for letters in phrase:
+    reversed = "" + reversed
+
+print(reversed) 
