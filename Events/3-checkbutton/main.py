14f92a489477ae3430421fbbab10cc7b706670a8
BaDMaN90 <56019790+BaDMaN90@users.noreply.github.com>
2019-11-21 10:13:07 +0000

    Update main.py

--- Events/3-checkbutton/main.py
+++ Events/3-checkbutton/main.py
@@ -10,12 +10,21 @@ class Gui(Tk):
         self.title("Passport Checker")
         # add variables
         self.check_var_1 = IntVar()
+        self.check_var_2 = IntVar()
+        self.check_var_3 = IntVar()
 
+        
         # add components
         self.__add_heading_label()
         self.__add_instruction_label_1()
         self.__add_yes_checkbox_1()
         self.__add_no_checkbox_1()
+        self.__add_instruction_label_2()
+        self.__add_yes_checkbox_2()
+        self.__add_no_checkbox_2()
+        self.__add_instruction_label_3()
+        self.__add_yes_checkbox_3()
+        self.__add_no_checkbox_3()
         self.__add_check_button()
 
         
@@ -30,7 +39,7 @@ class Gui(Tk):
     def __add_instruction_label_1(self):
         # create
         self.add_instruction_label_1 = Label()
-        self.add_instruction_label_1.grid(sticky ="w",row=1, column=0)
+        self.add_instruction_label_1.grid(columnspan=2,sticky ="w",row=1, column=0)
         # style
         self.add_instruction_label_1.configure(font = "Arial 14",text="1. Photo matches face?")
 
@@ -47,17 +56,68 @@ class Gui(Tk):
         self.add_no_checkbox_1.grid(row = 2, column = 1)
         #style
         self.add_no_checkbox_1.configure(text = "No", variable = self.check_var_1,onvalue = 0, offvalue = 1, height=2, width = 2)
+
+    def __add_instruction_label_2(self):
+        # create
+        self.add_instruction_label_2 = Label()
+        self.add_instruction_label_2.grid(columnspan=2,sticky ="w",row=3, column=0)
+        # style
+        self.add_instruction_label_2.configure(font = "Arial 14",text="2. Passport has at least 6 months validity?")
+
+    def __add_yes_checkbox_2(self):
+        #create
+        self.add_yes_checkbox_2 = Checkbutton()
+        self.add_yes_checkbox_2.grid(row = 4, column = 0)
+        #style
+        self.add_yes_checkbox_2.configure(text = "Yes", variable = self.check_var_2,onvalue = 1, offvalue = 0, height=2, width = 2)
+
+    def __add_no_checkbox_2(self):
+        #create
+        self.add_no_checkbox_2 = Checkbutton()
+        self.add_no_checkbox_2.grid(row = 4, column = 1)
+        #style
+        self.add_no_checkbox_2.configure(text = "No", variable = self.check_var_2,onvalue = 0, offvalue = 1, height=2, width = 2)
+
+    def __add_instruction_label_3(self):
+        # create
+        self.add_instruction_label_3 = Label()
+        self.add_instruction_label_3.grid(columnspan=2,sticky ="w",row=5, column=0)
+        # style
+        self.add_instruction_label_3.configure(font = "Arial 14",text="3. Visa, if required, is valid?")
+
+    def __add_yes_checkbox_3(self):
+        #create
+        self.add_yes_checkbox_3 = Checkbutton()
+        self.add_yes_checkbox_3.grid(row = 6, column = 0)
+        #style
+        self.add_yes_checkbox_3.configure(text = "Yes", variable = self.check_var_3,onvalue = 1, offvalue = 0, height=2, width = 2)
+
+    def __add_no_checkbox_3(self):
+        #create
+        self.add_no_checkbox_3 = Checkbutton()
+        self.add_no_checkbox_3.grid(row = 6, column = 1)
+        #style
+        self.add_no_checkbox_3.configure(text = "No", variable = self.check_var_3,onvalue = 0, offvalue = 1, height=2, width = 2)   
+        
     def __add_check_button(self):
         # create
         self.check_button = Button()
-        self.check_button.grid(columnspan=2,row=3, column=0)
+        self.check_button.grid(columnspan=2,row=8, column=0)
         # style
         self.check_button.configure(bd=4,text="Check",width = 10)
         # events
         self.check_button.bind("<ButtonRelease-1>", self.__check_button_clicked)
+
+    
         
     def __check_button_clicked(self, event):
-        pass
+        if self.check_var_1.get() == 1 and self.check_var_2.get() == 1 and self.check_var_3.get() == 1:
+            info_message = "Passport is Valid."
+            messagebox.showinfo("Passport Result", info_message)
+        else:
+            info_message = "Passport is Not Valid."
+            messagebox.showinfo("Passport Result", info_message)
+
 
 gui = Gui()
 gui.mainloop()  
