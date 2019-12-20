f7d55d9061211a1ad86321e801dcf986be747987
BaDMaN90 <4zdebf99@solent.ac.uk>
2019-12-20 11:06:31 +0000

    ver 1.01

--- Basic/5-functions/7-return-values/bot.py
+++ Basic/5-functions/7-return-values/bot.py
@@ -1,13 +1,20 @@
 def sum_weights(beep_weight, bop_weight):
     print("their sum weight is:",beep_weight + bop_weight)
+    totalweight = beep_weight + bop_weight
+    return totalweight
+    
 def calc_avg_weight(beep_weight, bop_weight):
-    print("their ave weight is:",(beep_weight + bop_weight)/2)
+    average = sum_weights(beep_weight, bop_weight)/2
+    print("their ave weight is:",average)
+    
 def run():
     bot_one_weight = int(input("what is beep weight?\n"))
     bot_two_weight = int(input("what is bop weight?\n"))
     function = input("what function should i run? 'sum' or 'ave'\n")
+    
     if function == "sum":
         sum_weights(bot_one_weight,bot_two_weight)
+        
     elif function == "ave":
         calc_avg_weight(bot_one_weight,bot_two_weight)
 
